862e688b7211cf2d6cce8b4886212cda4e97c8ae
Shayma Mukaram <shaymamukaram@gmail.com>
2018-09-12 08:03:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const currentUser = 'Grace Hopper';
  
 const welcomeMessage = 'Welcome to Flatbook, ';
+typeof "const currentUser = 'Grace Hopper';";
+//=> "string"
+ 
+"const currentUser = 'Grace Hopper';".length;
+//=> 35
+ 
+currentUser;
